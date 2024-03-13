c1ae6ac6506af866be55fc969fb4518c26f326ba
Dmitry Krutikov <d.krutikov@gmail.com>
2024-03-13 19:08:56 +0300

    Fix.

--- apps/worker-scraper/src/worker-scraper.processor.ts
+++ apps/worker-scraper/src/worker-scraper.processor.ts
@@ -74,17 +74,18 @@ const scraperRequest = async (
     const { statusCode, body } = await gotScraping.get({
       proxyUrl,
       url: avitoUrl,
-      headerGeneratorOptions: {
-        browsers: [
-          {
-            name: 'firefox',
-            minVersion: 120,
-            maxVersion: 123
-          }
-        ],
-        devices: ['desktop'],
-        locales: ['ru-RU'],
-        operatingSystems: ['windows']
+      headers: {
+          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "accept-language": "ru,en;q=0.9",
+          "cache-control": "max-age=0",
+          "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"YaBrowser\";v=\"23\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1"
       },
       responseType: 'buffer',
       followRedirect: false,
@@ -94,7 +95,7 @@ const scraperRequest = async (
     })
 
     if (statusCode !== 200) {
-      console.error(`Scraper response not success: ${statusCode}`)
+      console.error(`Scraper response not success: ${statusCode} via proxy ${proxyUrl}`)
     }
 
     return statusCode === 200 ? body : undefined
